223ed5a517bafe6f3ce1d5315f759a2138e5cf75
minimium <umekichi3hoshi@gmail.com>
2026-07-18 21:47:01 +0900

    Update init.lua

--- lua/meiro/init.lua
+++ lua/meiro/init.lua
@@ -5,7 +5,7 @@ vim.g.loaded_meiro = true
 
 -- :Meiro コマンドの登録 (引数で easy, normal, hard を指定可能)
 vim.api.nvim_create_user_command("Meiro", function(opts)
-  require("meiro_game").start(opts.args)
+  require("meiro").start(opts.args)
 end, {
   nargs = "?",
   desc = "Neovimの操作に慣れるための迷路ゲーム",
